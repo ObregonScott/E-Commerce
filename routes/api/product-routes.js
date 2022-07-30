f7a4a3295de789f51a3179a452dfba959ad5dbf2
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 01:07:29 -0500

    added routes for model, router delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,18 +22,18 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+
   Product.findOne(
-    {include: [{
-    model: Category, 
-  }, {
-    model: Tag
-  }], 
-    where: {
-      id: req.params.id
-    }
-  })
+    {
+      include: [{
+        model: Category,
+      }, {
+        model: Tag
+      }],
+      where: {
+        id: req.params.id
+      }
+    })
     .then(dbProductData => {
       if (!dbProductData) {
         res.status(404).json({ message: 'No product found with this id' });
@@ -122,7 +122,21 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {  id: req.params.id    }}
+    )
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
 module.exports = router;
