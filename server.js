c2b86784fe03dbe8282604cc9b34bb775cb04a26
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 01:55:16 -0500

    port and express

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+// Express Port Job Middleware?
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
