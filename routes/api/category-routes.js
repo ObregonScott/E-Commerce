13853d1a1a9672b7da7ea495016858bc8ae297c7
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 01:23:26 -0500

    gotta commit

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,8 +19,6 @@ router.get('/', (req, res) => {
 
 // Get One Category
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products ??
   Category.findOne({
     include: [{
       model: Product
@@ -54,12 +52,45 @@ router.post('/', (req, res) => {
     });
 });
 
+// Update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData[0]) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// Delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-
 module.exports = router;
