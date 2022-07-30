7df6cea9b5c897bc5ede9d53f91b7bdc3ef42a1e
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 01:24:27 -0500

    started tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// Find All Tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  
+    Tag.findAll()
+      .then(dbTagData => res.json(dbTagData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      })
+
 });
 
 router.get('/:id', (req, res) => {
