04ffcdd90ef3ad37d11fc7cb1f21f187165e8950
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 01:26:24 -0500

    create new tag, find tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,23 +5,50 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // Find All Tags
 router.get('/', (req, res) => {
-  
-    Tag.findAll()
-      .then(dbTagData => res.json(dbTagData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      })
+
+  Tag.findAll()
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 
 });
 
+// Find a single tag by its `id`
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+
+  Tag.create({
+    id: req.body.id,
+    tag_name: req.body.tag_name
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
