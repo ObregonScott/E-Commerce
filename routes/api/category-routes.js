b045170ecbfde93f22805e02273f7b3d181625b1
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 01:15:37 -0500

    working on category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,43 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// All Categories
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [{
+      model: Product
+    }]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
+// Get One Category
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  // be sure to include its associated Products ??
+  Category.findOne({
+    include:[{
+      model: Product
+    }],
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
